5c7b5e24023429f17ede7518701c6039ec04859f
Kevyn <kevynmarciano426@gmail.com>
2022-08-09 11:17:14 -0300

    ..

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const btnMobile = document.getElementById("btn-mobile");
+const menuA = document.getElementById("menuAToggle");
 
 function toggleMenu(event){
     if(event.type ==='touchstart') event.prevent.Default();
@@ -8,7 +9,13 @@ function toggleMenu(event){
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
 
-
+function menuAtoggle(event){
+    if(event.type ==='touchstart') event.prevent.Default();
+    const nav = document.getElementById("nav");
+    nav.classList.toggle('active');
+}
+menuA.addEventListener('click', toggleMenu);
+menuA.addEventListener('touchstart', toggleMenu);
 const titulo = document.querySelectorAll('#formacao h1');
 Array.from(titulo).forEach(typeWriter);
 
