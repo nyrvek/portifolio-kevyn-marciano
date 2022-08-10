487ca2b4cf26c2bac394b4b79c1f311bead2f41c
Kevyn <kevynmarciano426@gmail.com>
2022-08-09 21:10:14 -0300

    organização olhar url da imagem construção no css

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const btnMobile = document.getElementById("btn-mobile");
+const menuA = document.getElementById("menuAToggle");
+
+function toggleMenu(event) {
+  if (event.type === "touchstart") event.prevent.Default();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+}
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
+
+function menuAtoggle(event) {
+  if (event.type === "touchstart") event.prevent.Default();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
+}
+menuA.addEventListener("click", toggleMenu);
+menuA.addEventListener("touchstart", toggleMenu);
+const titulo = document.querySelectorAll("#formacao h1");
+Array.from(titulo).forEach(typeWriter);
+
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => {
+      elemento.innerHTML += letra;
+    }, 100 * i);
+  });
+}
